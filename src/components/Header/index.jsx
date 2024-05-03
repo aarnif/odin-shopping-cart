07663cf2b78985940c4ac35eca2799a510986bed
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-05-03 11:46:13 +0300

    Display header based on scrolling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,26 @@
 import LinkItem from "./LinkItem";
+import { useScroll, useMotionValueEvent, motion } from "framer-motion";
+import { useState } from "react";
 
 const Header = ({ shoppingCart }) => {
+  const { scrollY } = useScroll();
+  const [showHeader, setShowHeader] = useState(true);
+  const headerHeightInPixels = 60;
+
+  useMotionValueEvent(scrollY, "change", () => {
+    console.log("Scroll Y:", scrollY.current);
+    if (
+      scrollY.current > scrollY.prev &&
+      scrollY.current >= headerHeightInPixels
+    ) {
+      console.log("Page scrolling down");
+      setShowHeader(false);
+    } else {
+      console.log("Page scrolling up");
+      setShowHeader(true);
+    }
+  });
+
   const shoppingCartItemCount = shoppingCart.reduce(
     (sum, item) => sum + Number(item.quantity),
     0
@@ -9,7 +29,16 @@ const Header = ({ shoppingCart }) => {
   console.log("Shopping cart item count:", shoppingCartItemCount);
 
   return (
-    <header className="w-full py-4 px-12 flex justify-center items-center">
+    <motion.header
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: -headerHeightInPixels },
+      }}
+      animate={showHeader ? "visible" : "hidden"}
+      transition={{ ease: "easeInOut", duration: 0.3 }}
+      className="fixed w-full flex justify-center items-center bg-slate-300"
+      style={{ height: headerHeightInPixels }}
+    >
       <nav className="flex-grow flex justify-between items-center">
         <h1 className="flex-grow text-xl text-center">Art Store</h1>
         <ul className="max-w-[600px] flex-grow flex justify-around items-center">
@@ -23,7 +52,7 @@ const Header = ({ shoppingCart }) => {
           />
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
